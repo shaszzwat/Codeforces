9996d3230b041b00e3e3bf781daa6b9eccf6640d
Shashwat karn <shashwatkarn200512@gmail.com>
2026-06-23 23:27:28 +0530

    Add Sublime Sequence [A] from Codeforces

---
+++ 2148/A - Sublime Sequence/solution.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+    while(a--){
+        int x,n;
+        cin>>x>>n;
+        if(n%2==0){
+            cout<<0<<"
+";
+        }
+        else{
+            cout<<x<<"
+";
+        }
+    }
+}
