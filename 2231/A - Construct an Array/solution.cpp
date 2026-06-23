d65c8376d5ee17e48b24c10ef801f749ed84e315
Shashwat karn <shashwatkarn200512@gmail.com>
2026-06-23 23:34:38 +0530

    Add Construct an Array [A] from Codeforces

---
+++ 2231/A - Construct an Array/solution.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    while(n--){
+        int t;
+        cin>>t;
+        vector<int>v(t);
+        for(int i=0;i<t;i++){
+            cout<<2*i+1<<" ";
+ 
+ 
+    }
+    cout<<endl;
+ 
+    
+    }
+ 
+}
